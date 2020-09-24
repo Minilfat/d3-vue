ecc997be666ebab39b60771accd696dfdf284da6
Ilfat Mindubaev <yagarantuz@gmail.com>
2020-09-25 01:03:57 +0300

    Zoom to state boundaries on state click

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,12 +3,15 @@
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
 import topologyMap from "./russia.json";
 
 const w = 954;
 const h = 560;
+
+const maxScale = 100;
 
 export default {
   name: "Map",
@@ -17,6 +20,8 @@ export default {
   },
   data() {
     return {
+      zoom: null,
+      zoomedNode: null,
       svg: null,
       svgG: null,
       projection: null,
@@ -42,33 +47,13 @@ export default {
         .style("fill", "red")
         .style("cursor", "pointer")
         .attr("class", "city")
-        .attr("transform", (d) => "translate(" + this.projection([d.location.longitude, d.location.latitude]) + ")")
-        .on("click", clicked);
-
-      const vm = this;
-
-      function clicked(e, d) {
-        const [toX, toY] = vm.projection([d.location.longitude, d.location.latitude]);
-        let k = 1,
-          x = 0,
-          y = 0;
-
-        if (vm.navigatedPoint && vm.navigatedPoint[0] === toX && vm.navigatedPoint[1] === toY) {
-          vm.$set(vm, "navigatedPoint", null);
-        } else {
-          [x, y] = [toX, toY];
-          k = 2;
-          vm.$set(vm, "navigatedPoint", [x, y]);
-        }
-
-        vm.svgG
-          .transition()
-          .duration(750)
-          .attr("transform", "translate(" + -x + "," + -y + ")scale(" + k + ")");
-      }
+        .attr("transform", (d) => "translate(" + this.projection([d.location.longitude, d.location.latitude]) + ")");
     },
 
     createMap() {
+      const states = topojson.feature(topologyMap, topologyMap.objects.russia);
+      const vm = this;
+
       this.projection = d3
         .geoAlbers()
         .rotate([-105, 0])
@@ -84,15 +69,69 @@ export default {
         .attr("width", w + "px")
         .attr("height", h + "px");
 
+      this.zoom = d3
+        .zoom()
+        .scaleExtent([1, maxScale])
+        .on("zoom", zoomed);
+
       this.svgG = this.svg.append("g");
       this.svgG
         .attr("class", "region")
         .selectAll("path")
-        .data(topojson.feature(topologyMap, topologyMap.objects.russia).features)
+        .data(states.features)
         .enter()
         .append("path")
-        .attr("d", this.path);
+        .attr("d", this.path)
+        .on("click", function(e, d) {
+          vm.regionClicked(this, d, vm);
+        });
+
+      function zoomed({transform}) {
+        vm.svgG.selectAll("circle.city").attr("r", 5 / transform.k);
+        vm.svgG.attr("transform", transform);
+      }
     },
+
+    regionClicked(node, d, vm) {
+      if (vm.zoomedNode && vm.zoomedNode === node) {
+        vm.$set(vm, "zoomedNode", null);
+        vm.svgG
+          .transition()
+          .duration(750)
+          .call(vm.zoom.transform, d3.zoomIdentity)
+          .on("end", resetVisibility);
+
+        return;
+      }
+
+      vm.$set(vm, "zoomedNode", node);
+
+      const bounds = vm.path.bounds(d),
+        dx = bounds[1][0] - bounds[0][0],
+        dy = bounds[1][1] - bounds[0][1],
+        x = (bounds[0][0] + bounds[1][0]) / 2,
+        y = (bounds[0][1] + bounds[1][1]) / 2,
+        scale = Math.max(1, Math.min(maxScale, 0.9 / Math.max(dx / w, dy / h))),
+        translate = [w / 2 - scale * x, h / 2 - scale * y];
+
+      // vm.projection.fitSize([x, y], d);
+
+      vm.svgG
+        .transition()
+        .duration(750)
+        .call(vm.zoom.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale))
+        .on("end", resetVisibility);
+
+      function resetVisibility() {
+        vm.svgG.selectAll(".city").classed("hidden", !!vm.zoomedNode);
+        vm.svgG.selectAll(".region path").classed("hidden", function(data) {
+          // if (data.properties.region === d.properties.region) console.log("hui");
+          // console.log(vm.zoomedNode);
+          return !!vm.zoomedNode && data.properties.region !== d.properties.region;
+        });
+      }
+    },
+
     addBg() {
       const grad = this.svg
         .append("defs")
@@ -135,5 +174,16 @@ export default {
 <style>
 #map .region {
   fill: url(#gradient);
+}
+
+#map .city,
+#map .region path {
+  opacity: 1;
+  transition: opacity 400ms ease-in;
+}
+
+#map .city.hidden,
+#map .region path.hidden {
+  opacity: 0.3;
 }
 </style>
